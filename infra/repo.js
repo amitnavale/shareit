957c6f652895fc6b75c944057f5bee723e535e2f
Amit <Navale>
2016-09-01 17:40:15 +1000

    grid-header fixed

--- infra/repo.js
+++ infra/repo.js
@@ -2,5 +2,11 @@ var  repo ={
         members: [ {name: 'John', id:1, mode: ''},
                     {name: 'Simon', id:2, mode: ''}],
         events: [{name: 'Johns BirthDay', id:1, mode: '' }],
-        expenses: [{amount: 20, expenseBy: 1, expenseFor: [1,2], mode:''}]
+        expenses: [{amount: 20, 
+            expenseBy: 1,
+            expenseByName: 'John',
+            expenseFor: [1,2],
+            expenseForName: ['John', 'Simon'],
+            description: 'some random party',
+            mode:''}]
     };
