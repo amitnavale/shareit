4c12a243b3e153650d2d2aca15b4454534377dc0
Amit <Navale>
2016-09-01 16:59:51 +1000

    members done

--- infra/repo.js
+++ infra/repo.js
@@ -1,5 +1,5 @@
 var  repo ={
-        persons: [ {name: 'John', id:1, mode: ''},
+        members: [ {name: 'John', id:1, mode: ''},
                     {name: 'Simon', id:2, mode: ''}],
         events: [{name: 'Johns BirthDay', id:1, mode: '' }],
         expenses: [{amount: 20, expenseBy: 1, expenseFor: [1,2], mode:''}]
