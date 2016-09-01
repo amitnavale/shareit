a89e5df624e7171ad84eda7fd9e284e74019c29f
Amit <Navale>
2016-09-01 16:42:46 +1000

    expense base line

--- infra/repo.js
+++ infra/repo.js
@@ -2,5 +2,5 @@ var  repo ={
         persons: [ {name: 'John', id:1, mode: ''},
                     {name: 'Simon', id:2, mode: ''}],
         events: [{name: 'Johns BirthDay', id:1, mode: '' }],
-        expense: [{amount: 20, expenseBy: 1, expenseFor: [1,2], mode:''}]
+        expenses: [{amount: 20, expenseBy: 1, expenseFor: [1,2], mode:''}]
     };
