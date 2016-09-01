efa426ccd1f3c982cd76458e1b991050f5ff0ca6
Amit <Navale>
2016-09-01 16:34:14 +1000

    base line commit

---
+++ infra/repo.js
@@ -0,0 +1,6 @@
+var  repo ={
+        persons: [ {name: 'John', id:1, mode: ''},
+                    {name: 'Simon', id:2, mode: ''}],
+        events: [{name: 'Johns BirthDay', id:1, mode: '' }],
+        expense: [{amount: 20, expenseBy: 1, expenseFor: [1,2], mode:''}]
+    };
