f51297654bfad8aef4b065f5282b06bf0509d42a
Amit <Navale>
2016-10-08 19:42:57 +1100

    link event and expense

--- infra/repo.js
+++ infra/repo.js
@@ -1,16 +1,7 @@
-var repo ={
-        members: [ {name: 'John', id:1, mode: ''},
-                    {name: 'Simon', id:2, mode: ''},
-                    {name: 'Dave', id:3, mode: ''},
-                    {name: 'Elizabeth', id:4, mode: ''}
-                    ],
-        events: [{name: 'Johns BirthDay', id:1, mode: '' }],
-        expenses: [{id: 1,
-            amount: 20, 
-            expenseBy: 1,
-            expenseByName: 'John',
-            expenseFor: [1,2],
-            expenseForName: ['John', 'Simon'],
-            description: 'some random party',
-            mode:''}]
-    };
+var repo = {
+    members: [{name: 'John', id: 1, mode: ''}, {name: 'Simon', id: 2, mode: ''}, {name: 'Dave', id: 3, mode: ''}, {name: 'Elizabeth', id: 4, mode: ''}],
+    events: [{name: 'event 1', id: 1, mode: '' }, {name: 'event 2', id: 2, mode: '' }],
+    expenses: [
+        { eventId: 1, id: 1, amount: 20, expenseBy: 1, expenseByName: 'John', expenseFor: [1, 2], expenseForName: ['John', 'Simon'], description: 'event 1', mode: ''}, 
+        { eventId: 2, id: 1, amount: 20, expenseBy: 1, expenseByName: 'John', expenseFor: [1, 2], expenseForName: ['John', 'Simon'], description: 'event 2', mode: ''}]
+};
